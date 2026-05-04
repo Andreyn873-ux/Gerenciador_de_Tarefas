385a58535c1aee812c4a5cb282c739da34eadefc
Andrey nicolas <andrey873@gmail.com>
2026-05-03 21:44:16 -0300

    fix: adiciona dependencias e inicializa banco no deploy

--- api.py
+++ api.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from Controllers.controllers import tarefas_bp, init_routes
+from database import criar_tabelas
 from repositories.tarefas_repository import TarefasRepository
 from services.tarefas_service import TarefasService
 
 app = Flask(__name__)
+
+criar_tabelas()
 
 repository = TarefasRepository()
 service = TarefasService(repository)
